986227b63635d5254f12238998e3c8ff35be3bdd
Yuan <wang17@ualberta.ca>
2020-12-23 18:24:44 +0800

    Add like and comment buttons

--- pages/home.js
+++ pages/home.js
@@ -3,7 +3,8 @@ import Head from 'next/head';
 import axios from 'axios';
 import useSWR from 'swr';
 import ReactMarkDown from "react-markdown";
-import { message, Layout, List, Avatar, Spin, Tag} from 'antd';
+import { message, Layout, List, Avatar, Spin, Tag, Drawer} from 'antd';
+import { MessageOutlined,LikeOutlined } from "@ant-design/icons";
 import { useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import Header from '../components/header';
@@ -16,6 +17,7 @@ const Home = () => {
     const router = useRouter();
     const isLogged = useSelector((state) => state.isLogged);
     const username = useSelector((state) => state.username);
+    const [visible, setVisible] = useState(false);
 
     useEffect(() => {
         if (!isLogged) {
@@ -40,7 +42,7 @@ const Home = () => {
     return (
         <div>
             <Head>
-                <title>Home Page</title>
+                <title>Home</title>
                 <meta
                     name="viewport"
                     content="initial-scale=1.0, width=device-width"
@@ -51,44 +53,69 @@ const Home = () => {
                     <Header selectedKey={["1"]} />
                     <div className="postList" >
                         {!visiblePosts ? <Spin tip="Loading posts ... " /> :
-                            <List
-                                itemLayout="vertical"
-                                dataSource={visiblePosts}
-                                size="large"
-                                renderItem={item => (
-                                <List.Item>
-                                    <List.Item.Meta
-                                    avatar={<Avatar src="https://avatars1.githubusercontent.com/u/8186664?s=460&v=4" />}
-                                    title={
-                                        <a>{item.author.username}</a> 
-                                    }
-                                    description={
-                                        <div className="postDescription" >
-                                            {item.createdAt}
-                                            <br />
-                                            {item.tags.map((cat, idx) => (
-                                                <a>
-                                                    <Tag key={idx} color={randomColor()}>
-                                                    {cat}
-                                                    </Tag>
-                                                </a>
+                            <div>
+                                 <List
+                                    itemLayout="vertical"
+                                    dataSource={visiblePosts}
+                                    size="large"
+                                    renderItem={item => (
+                                    <List.Item
+                                        key={item.id}
+                                        actions={[
+                                            <a className="feedbackButton" >
+                                                <LikeOutlined className="feedbackButton"/>
+                                                {item.likes}
+                                            </a >,
+                                            <a  className="feedbackButton"
+                                                onClick={() => setVisible(true)} 
+                                            >
+                                            <MessageOutlined className="feedbackButton" />
+                                            {item.comments}
+                                            </a>,
+                                        ]}
+                                    >
+                                        <List.Item.Meta
+                                            avatar={<Avatar size={50} src="https://avatars1.githubusercontent.com/u/8186664?s=460&v=4" />}
+                                            title={
+                                                <a>{item.author.username}</a> 
+                                            }
+                                            description={
+                                                <div className="postDescription" >
+                                                    {item.createdAt}
+                                                    <br />
+                                                    {item.tags.map((cat, idx) => (
+                                                        <a>
+                                                            <Tag key={idx} color={randomColor()}>
+                                                                {cat}
+                                                            </Tag>
+                                                        </a>
+                                                    ))}
+                                                </div>
                                                 
-
-                                            ))}
+                                            }
+                                        />
+                                        <div style={{ overflow: "auto" }}>
+                                            <h2>{item.title}</h2>
+                                            {item.contentType === "text" ? (
+                                                item.content) : 
+                                                (<ReactMarkDown source={item.content} />)
+                                            }
                                         </div>
-                                        
-                                    }
-                                    />
-                                    <div style={{ overflow: "auto" }}>
-                                        <h2>{item.title}</h2>
-                                        {item.contentType === "text" ? (
-                                            item.content) : 
-                                            (<ReactMarkDown source={item.content} />)
-                                        }
-                                    </div>
-                                </List.Item>
-                                )}
-                            />
+                                    </List.Item>
+                                    )}
+                                />
+                                <Drawer
+                                    title="Comments"
+                                    placement="right"
+                                    closable={true}
+                                    onClose={() => setVisible(false)}
+                                    visible={visible}
+                                >
+                                    <p>Some contents...</p>
+                                    <p>Some contents...</p>
+                                    <p>Some contents...</p>
+                                </Drawer>
+                            </div>
                         }
                     </div>
                     <Footer className="pageFooter">Keep In Touch ©2020 Created by Yuan Wang</Footer>
